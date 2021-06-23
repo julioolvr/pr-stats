51c778759761bfddee1c908d05b6ee7c2e1fd434
Julio <julio.olvr@gmail.com>
2021-06-23 12:26:17 +0200

    Login with auth0

---
+++ src/auth/AuthProvider.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+export default function Auth0ProviderWithHistory({ children }: Props) {
+  // TODO: Check properly that these are always present
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  const history = useHistory();
+
+  const onRedirectCallback = (appState: any) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain!}
+      clientId={clientId!}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+}
+
+type Props = {
+  children: React.ReactNode;
+};
