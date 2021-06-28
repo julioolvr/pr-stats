55b73e55c8287ec7d399fb9a84af1b8a7d519ac5
Julio <julio.olvr@gmail.com>
2021-06-28 09:44:30 +0200

    Request PRs from Github

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Octokit } from "octokit";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
 
 import AuthProvider from "./auth/AuthProvider";
 import LoginButton from "./auth/LoginButton";
+import getTimeToMerge from "./modules/timeToMerge";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <div>hi</div>
-        <LoginButton />
+        <QueryClientProvider client={queryClient}>
+          <div>hi</div>
+          <PrListTest />
+          <LoginButton />
+        </QueryClientProvider>
       </AuthProvider>
     </Router>
   );
 }
 
 export default App;
+
+const OWNER = "julioolvr";
+const REPO = "slack-estimation";
+const COUNT = 10;
+
+function PrListTest() {
+  const query = useQuery(["prs", OWNER, REPO, COUNT], () =>
+    getTimeToMerge({ owner: OWNER, repo: REPO, count: COUNT })
+  );
+
+  return <div>Data!</div>;
+}
---
+++ src/modules/timeToMerge.ts
@@ -0,0 +1,114 @@
+import { Octokit } from "octokit";
+import * as dateFns from "date-fns";
+
+export default async function getTimeToMerge({ owner, repo, count }: Options) {
+  const prs = await listPrs({ owner, repo, count });
+  console.log(prs);
+}
+
+type Options = {
+  owner: string;
+  repo: string;
+  count: number;
+};
+
+async function listPrs({ owner, repo, count }: ListPrsOptions) {
+  const githubClient = new Octokit();
+
+  // TODO: Think about how it should work when a PR goes back and forth
+  // multiple times between draft and not draft.
+  // TODO: Can I assume ConvertToDraft and ReadyForReview will always
+  // come in pairs?
+  const response = await githubClient.graphql<{
+    repository: {
+      pullRequests: {
+        edges: [{ node: PullRequestResponse }];
+      };
+    };
+  }>(
+    `
+    repository(owner: $owner, repo: $repo) {
+      pullRequests(last: $count, states: CLOSED) {
+        edges {
+          node {
+            number
+            title
+            createdAt
+            mergedAt
+            timelineItems(last: 1, itemTypes: [READY_FOR_REVIEW_EVENT]) {
+              edges {
+                node {
+                  ... on ReadyForReviewEvent {
+                    createdAt
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+    {
+      count,
+      owner,
+      repo,
+    }
+  );
+
+  return response.repository.pullRequests.edges.map((edge) =>
+    parsePrResponse(edge.node)
+  );
+}
+
+type ListPrsOptions = {
+  owner: string;
+  repo: string;
+  count: number;
+};
+
+type PullRequestResponse = {
+  number: number;
+  title: string;
+  createdAt: string;
+  mergedAt: string;
+  timelineItems: [
+    {
+      edges: [
+        {
+          node: {
+            createdAt: string;
+          };
+        }
+      ];
+    }
+  ];
+};
+
+type PullRequest = {
+  number: number;
+  title: string;
+  createdAt: Date;
+  mergedAt: Date;
+  reviewRequestedDate: Date;
+};
+
+function parsePrResponse(pr: PullRequestResponse): PullRequest {
+  // The assumption here is that if a PR either never had a `ReadyForReview`
+  // event then it wasn't created as a draft. Then we care about the PR's
+  // createdAt date.
+  // If the PR _has_ a ReadyForReview event, the GraphQL query will return the
+  // last one, and for now that's the one we care about to know how long it took
+  // since _then_ until merge.
+  const reviewRequestedDate = dateFns.parseISO(
+    pr.timelineItems[0]?.edges[0]?.node?.createdAt ?? pr.createdAt
+  );
+
+  return {
+    number: pr.number,
+    title: pr.title,
+    createdAt: dateFns.parseISO(pr.createdAt),
+    mergedAt: dateFns.parseISO(pr.mergedAt),
+    reviewRequestedDate,
+  };
+}
