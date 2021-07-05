3fa4abfd2c7b33accd67f332c25e821b307d8859
Julio <julio.olvr@gmail.com>
2021-07-05 22:23:57 +0200

    Update PrStats page

--- src/pages/PrStats.tsx
+++ src/pages/PrStats.tsx
@@ -2,32 +2,36 @@ import { RouteComponentProps } from "react-router-dom";
 import { useQuery } from "react-query";
 import { BarChart, Bar, LabelList } from "recharts";
 import {
-  Box,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
   Spinner,
+  Heading,
+  Text,
+  Container,
+  Stack,
+  SimpleGrid,
+  Box,
+  Stat,
+  StatLabel,
+  StatNumber,
 } from "@chakra-ui/react";
 
 import useGithubToken from "auth/useGithubToken";
 import getTimeToMerge from "modules/timeToMerge";
 
 const COUNT = 10;
+const NUMBER_FORMAT = new Intl.NumberFormat("en", { maximumFractionDigits: 0 });
 
 export default function PrListTest({
   match,
 }: RouteComponentProps<{ owner: string; repository: string }>) {
   const token = useGithubToken();
+  const { owner, repository } = match.params;
 
   const query = useQuery(
-    ["prs", match.params.owner, match.params.repository, COUNT, token],
+    ["prs", owner, repository, COUNT, token],
     async () =>
       await getTimeToMerge({
-        owner: match.params.owner,
-        repo: match.params.repository,
+        owner: owner,
+        repo: repository,
         count: COUNT,
         token: token!,
       }),
@@ -39,42 +43,54 @@ export default function PrListTest({
   }
 
   return (
-    <Box>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>Number of PRs</Th>
-            <Th>Total number of hours to merge</Th>
-            <Th>Average</Th>
-            <Th>P50</Th>
-            <Th>P75</Th>
-            <Th>P90</Th>
-            <Th>P99</Th>
-          </Tr>
-        </Thead>
+    <Container>
+      <Stack>
+        <Heading>
+          <Text fontSize="xl" color="gray.600">
+            {owner}
+          </Text>
+          {repository}
+        </Heading>
 
-        <Tbody>
-          <Tr>
-            <Td>{query.data.prs.length}</Td>
-            <Td>
-              {query.data.prs
-                .map((pr) => pr.hoursUntilMerge)
-                .reduce((a, b) => a + b, 0)}
-            </Td>
-            <Td>{query.data.stats.average}</Td>
-            <Td>{query.data.stats.p50}</Td>
-            <Td>{query.data.stats.p75}</Td>
-            <Td>{query.data.stats.p90}</Td>
-            <Td>{query.data.stats.p99}</Td>
-          </Tr>
-        </Tbody>
-      </Table>
+        <SimpleGrid columns={2} spacing={4}>
+          {/*
+            TODO: This is actually the number of PRs fetched and used for the
+            calculation, make that more clear.
+          */}
+          <StatBox title="Number of PRs" number={query.data.prs.length} />
+          <StatBox
+            title="Total number of hours until merge"
+            number={query.data.prs
+              .map((pr) => pr.hoursUntilMerge)
+              .reduce((a, b) => a + b, 0)}
+          />
+          <StatBox
+            title="Average hours until merge per PR"
+            number={query.data.stats.average}
+          />
+          <StatBox title="p50" number={query.data.stats.p50} />
+          <StatBox title="p75" number={query.data.stats.p75} />
+          <StatBox title="p90" number={query.data.stats.p90} />
+          <StatBox title="p99" number={query.data.stats.p99} />
+        </SimpleGrid>
 
-      <BarChart data={query.data.prs} width={730} height={250}>
-        <Bar dataKey="hoursUntilMerge" fill="#8884d8">
-          <LabelList dataKey="title" position="insideTop" angle={45} />
-        </Bar>
-      </BarChart>
+        <BarChart data={query.data.prs} width={730} height={250}>
+          <Bar dataKey="hoursUntilMerge" fill="#8884d8">
+            <LabelList dataKey="title" position="insideTop" angle={45} />
+          </Bar>
+        </BarChart>
+      </Stack>
+    </Container>
+  );
+}
+
+function StatBox({ title, number }: { title: string; number: number }) {
+  return (
+    <Box py={2} px={3} borderWidth={1} borderColor="gray.200" borderRadius="md">
+      <Stat>
+        <StatLabel color="gray.500">{title}</StatLabel>
+        <StatNumber>{NUMBER_FORMAT.format(number)}</StatNumber>
+      </Stat>
     </Box>
   );
 }
